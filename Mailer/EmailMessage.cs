c4393420109a966959df143541c587e4a483f549
Piotr Czarnik <czorny.baran@gmail.com>
2018-02-08 12:52:36 +0100

    Summary
    none

---
+++ Mailer/EmailMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mailer
+{
+    class EmailMessage
+    {
+        public string Receiver { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Mailer/MailSender.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace Mailer
+{
+    class MailSender : IDisposable
+    {
+        SmtpClient client;
+
+        public MailSender(MailSenderOptions options)
+        {
+
+            client = new SmtpClient();
+            // client.Connect(options.SmtpServer, options.Port, false);
+            // client.Authenticate(options.Username, options.Password);         
+        }
+
+        public void SendEmails(ICollection<EmailMessage> messages)
+        {
+            
+            foreach (var msg in messages)
+            {
+                SendMessage(msg);
+             
+            }
+
+        }
+
+        public void SendMessage(EmailMessage message /*MailSenderOptions options*/)
+        {
+            try
+            {
+                Console.WriteLine(message.Body);
+                var mimeMessage = new MimeMessage();
+                mimeMessage.From.Add(new MailboxAddress("myemail@gmail.com", message.Subject));
+                mimeMessage.To.Add(new MailboxAddress(message.Subject, message.Receiver));
+                mimeMessage.Body = new TextPart("Wiadomość: ")
+                {
+                    Text = message.Body
+                };
+               // client.Send(mimeMessage);
+               // Console.WriteLine("The mails has been sended");
+                Console.ReadLine();
+                
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+}
+
+public void Dispose()
+{
+    client.Disconnect(false);
+    client.Dispose();
+}
+    }
+}
